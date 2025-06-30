133f8d77d81e7225299cba4498c12c59de71fb64
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2025-06-29 17:30:03 -0700

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import join from './utils'
-import About from './components/about'
+// import About from './components/about'
+import Home from './components/home'
 import Header from './components/header'
 import Contact from './components/contact'
+import Navigation from './components/navigation'
+import Services from './components/servicesSection'
 
 const App: React.FC = () => {
+  const [route, setRoute] = React.useState('home')
   return (
-    <div className={join('text-center bg-background min-h-screen m-0 p-0')}>
-      <div className={join('md:flex sticky')}>
+    <div className={join('text-center pb-24', ' bg-background min-h-screen')}>
+      <div
+        className={join(
+          'flex flex-col md:flex-row',
+          'items-center justify-between px-6 py-4 w-full',
+        )}
+      >
         <Header />
+        <Navigation setRoute={setRoute} />
       </div>
-      <About />
+      {route === 'home' && <Home />}
+      {route === 'services' && <Services />}
       <Contact />
     </div>
   )
